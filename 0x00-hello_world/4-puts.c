b6384afe1187ca1a96174fcf0686e0c5b3d72dcd
kups23 <audualoma@gmail.com>
2022-12-10 14:39:25 -0800

    4-puts.c script print out strings

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/*
+ *Project print out on the screen
+ *It does not use printf
+*/
+int main()
+{
+	put("\"Programming is like building a multilingual puzzle"); //print out what is in bracket
+	return 0;
+}
+	
