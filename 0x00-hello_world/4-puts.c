851acbe188ffd031fa5aa96879516666cba1122e
kups23 <audualoma@gmail.com>
2022-12-10 15:48:00 -0800

    4-puts.c print out string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,12 @@
 /**
  * main - this is the entry point
  * Project prints out words
- * It does not use printf 
- *
+ * It does not use printf
+ * 
  * Return: a value 0
 */
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle"); 
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
