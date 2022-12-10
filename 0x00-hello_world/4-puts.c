96e0f0813e142b19ab982ea23f812fc43dff581f
kups23 <audualoma@gmail.com>
2022-12-10 14:46:38 -0800

    4-puts.c Print string of words

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * Project prints out words
+ * It does not use printf 
+*/
+
+int main ()
+{	put("\"Programming is like building a multilingual puzzle"); //Print out string of words
+	return 0;
+}
