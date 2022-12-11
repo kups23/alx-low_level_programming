7140677a82dc063d06d71c9e9d463dd4c55119a0
kups23 <audualoma@gmail.com>
2022-12-10 18:37:38 -0800

    101-quote.c prints out string

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - primary entry
+ *
+ * Return: value of 1 is returned
+*/
+int main(void)
+
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+
+}
