e5fe576123156bd345643458c134fb9367cebb58
kups23 <audualoma@gmail.com>
2022-12-12 04:42:05 -0800

    100-print_comb3.c print two digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point
+ * print two digit numbers separated with , and space
+ *
+ * Return: always return 0
+*/
+int main(void)
+{
+	int i;
+	int j;
+	
+	j = '0';
+	for ( i = '0'; i <= '9' i++)
+	{
+		while (j <= '9')
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if(i != '8' && j !='9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				j++;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
