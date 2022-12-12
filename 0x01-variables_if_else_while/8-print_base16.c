68f81abff4b8c5e1f78506027c6397b0ebb39429
kups23 <audualoma@gmail.com>
2022-12-12 03:35:27 -0800

    8-print_base16.c prints hexadecimal in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>.
+#include <unistd.h>
+
+/**
+ * main - entry point
+ * print hexadecimal number in lower case
+ *
+ * Return: always return 0
+*/
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = '0'; i < '10'; i++)
+	{
+		putchar(i);
+	}
+
+	j = 'a';
+	
+	while (j <= 'f')
+	{
+		putchar(j)
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
