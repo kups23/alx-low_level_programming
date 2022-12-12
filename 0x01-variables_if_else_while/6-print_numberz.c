92e03200e4d16dd00620c26fee180e06b08d9187
kups23 <audualoma@gmail.com>
2022-12-12 02:40:22 -0800

    6-print_numberz.c print base 10 numbers only with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main -  entry point
+ * print base 10 numbers
+ *
+ * Return: always return 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	purtchar('\n');
+	return (0);
+}
