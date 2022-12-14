b7e771bc51c445f42a17838cba2a83f8ead1ab50
kups23 <audualoma@gmail.com>
2022-12-14 15:01:04 -0800

    1-alphabet.c print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print alphabet
+ *
+ * Return: function does not return
+*/
+void print_alphabet(void)
+{
+	int ltr;
+
+	for (ltr = 'a'; ltr <= 'z'; ltr++)
+	{
+		_putchar(ltr);
+	}
+	_putchar(10);
+
+}
