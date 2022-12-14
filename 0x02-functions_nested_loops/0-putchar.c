96c07ef3f9be90e2c174ce7cc1457f314d39d73a
kups23 <audualoma@gmail.com>
2022-12-14 14:49:49 -0800

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
 */
 int main(void)
 {
-	char str[9] = "_putchar";
+	char str[10] = "_putchar";
 	int i;
 
 	for (i = 0; i < 8; i++)
 	{
 		_putchar(str[i]);
 	}
-	_putchar(9);
+	_putchar(10);
 	return (0);
 }	
