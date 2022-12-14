2457db22a4112675fcb04fef0ee1240f8f577e44
kups23 <audualoma@gmail.com>
2022-12-14 15:19:53 -0800

    2-print_alphabet_x10.c print letters 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ * print_alphabet_x10 - prints alphabet letters
+ * print it 10 times 
+ * return: now return
+*/
+void print_alphabet_x10(void)
+{
+	int counter, letters;
+
+	for (counter = 0; counter < 10; counter++)
+	{
+		for (letters = 'a';  letters <=  'z'; letters++)
+		{
+			_putchar(letters);
+		}
+		_putchar(10);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 # Describing the various program of the projects
 - 0-putchar.c program prints _putchar
-- 1-alphabet.c program prints alphabet in lowercase 
+- 1-alphabet.c program prints alphabet in lowercase
+-  2-print_alphabet_x10.c program prints alphabet 10 times 
