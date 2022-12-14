80f8ca03a1d541f4adcf36fe95c0161d92f4e6c3
kups23 <audualoma@gmail.com>
2022-12-14 13:59:43 -0800

    0-putchar.c print out string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point
+ * programs prints _putchar
+ *
+ * Return: always return (0)
+*/
+int main(void)
+{
+	char str[9] = "_putchar";
+	int i;
+
+	for (i = '0'; i <= '8' i++)
+	{
+		putchar(str[i]);
+	}
+	return (0);
+}	
