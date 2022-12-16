a86b3014fbe95d39f0c1d4784cf8699ba5109e7e
kups23 <audualoma@gmail.com>
2022-12-16 01:43:17 -0800

    5-more_numbers.c print numbers 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,12 +12,15 @@ void more_numbers(void)
 
 	while (tme < 10)
 	{
-		for (num =  '0'; num <= '14'; num++)
+		for (num =  0; num <= 14; num++)
 		{
-			_putchar(num);
+			if (num > 9)
+			{
+				_putchar((num / 10) + '0');
+			}
+			_putchar((num % 10) + '0');
 		}
-		_putchar('\n');
+		_putchar(10);
 		tme++;
 	}
-	_putchar('\n');
 }
