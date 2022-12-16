89285e284b882c9673fbfef215ca2f8ca02a4a52
kups23 <audualoma@gmail.com>
2022-12-16 09:18:30 -0800

    101-print_number.c print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,17 +8,15 @@
 */
 void print_number(int n)
 {
-	unsigned int i;
-	i = n;
-
+	unsigned int num = n;
 	if (n < 0)
 	{
-		_putchar(45);
-		i = -n;
-	}
-	if (i / 10)
+		_putchar('-');
+		num = -num;
+	}	
+	if (num > 9)
 	{
-		print_number(i / 10);
-		_putchar((i % 10) + '0');
+		print_number(num / 10);
 	}
+	_putchar(num % 10 + '0');
 }
