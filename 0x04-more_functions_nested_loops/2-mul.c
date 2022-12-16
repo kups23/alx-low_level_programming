0e38833af912b61c77fcdcb3951273724f49c358
kups23 <audualoma@gmail.com>
2022-12-16 00:50:29 -0800

    2-mul.c multiplies two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul  - funnction multiplies two intergers
+ * @a: number  to be multiplied
+ * @b: number to be multiplied
+ * Return: return the result
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,5 @@
 # Describing programs
 - 0-isupper.c program  checks for uppercase
 - 1-isdigit.c program checks for digit
+- 2-mul.c program multiplies two numbers
+
