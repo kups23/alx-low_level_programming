3972d82d3ae061c557196a8866df41d91fc6dae7
kups23 <audualoma@gmail.com>
2022-12-16 01:26:49 -0800

    5-more_numbers.c print numbers 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers -  print numbers 10 times 
+ *
+ * Return: returns nothing
+*/
+void more_numbers(void)
+{
+	int tme,num;
+	tme = 0;
+
+	while (tme < 10)
+	{
+		for (num =  '0'; num <= '14'; num++)
+		{
+			_putchar(num);
+			_putchar('\n');
+		}
+		tme++;
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -4,4 +4,5 @@
 - 2-mul.c program multiplies two numbers
 - 3-print_numbers.c prints number from 0 - 9
 - 4-print_most_numbers.c program prints numer from 0-9 except 2 and 4
+- 5-more_numbers.c program prints from 0 -14 ten times 
 
