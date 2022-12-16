36b4e75132de20e0ac17c53d6bfdd572bb4e4b2c
kups23 <audualoma@gmail.com>
2022-12-16 00:29:23 -0800

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,17 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
 /**
- * main.h file
+ * main - header file
+ * prototype of type void
 */
-
-void print_alphabet(void);
-void jack_bauer(void);
-void print_alphabet_x10(void);
-void times_table();
-void print_to_98(int);
-void print_times_table(int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 int _putchar(char);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int add(int n, int m);
-int print_last_digit(int n);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif /* MAIN_H */
