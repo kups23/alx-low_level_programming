bedd4b9dcbd8cc63e3e98bd7e86ddd54fb8fdd43
kups23 <audualoma@gmail.com>
2022-12-19 16:03:11 -0800

    1-swap.c program swaps two value

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/** wap_int - swaping values
+ * @a: first value 
+ * @b: second value
+ * Return: returns nothing
+*/
+void swap_int(int *a, int *b)
+{
+	int swap  = *a;
+	*a = *b;
+	*a =  swap;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 # Describing what programs are doing
-- 0-reset_to_98.c
+- 0-reset_to_98.c program changes value of pointer
+- 1-swap.c programs swaps two numbers
