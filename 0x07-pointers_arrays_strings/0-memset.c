f480335499f94c29f99e978767c426f0a3d3ca8d
kups23 <audualoma@gmail.com>
2023-01-02 15:22:31 -0800

    0-memset.c fill memmory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - functions fills with constant byte
+ * @s: pointe to char
+ * @b: constant byte
+ * @n: variable of type int
+ * Return: returns pointer s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned i;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
